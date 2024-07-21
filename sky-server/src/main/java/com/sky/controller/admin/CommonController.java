e7c6b4aa00372932da634d3c432b2d49220f9970
Fetters <997167305@qq.com>
2024-07-21 17:40:53 +0800

    新增菜品业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
 import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
@@ -34,7 +35,7 @@ public class CommonController {
      */
     @PostMapping("/upload")
     @ApiOperation("文件上传")
-    public Result<String> upload(@RequestBody MultipartFile file) {
+    public Result<String> upload(MultipartFile file) {
 
         log.info("文件上传: {}", file);
 
@@ -54,7 +55,7 @@ public class CommonController {
             log.error("文件上传失败: {}", e);
         }
 
-        return null;
+        return Result.error(MessageConstant.UPLOAD_FAILED);
     }
 
 }
