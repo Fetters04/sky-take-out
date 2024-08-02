5051483d5c83e5eec0deb0e69f74922d0e627411
Fetters <997167305@qq.com>
2024-08-02 20:58:23 +0800

    销量排名统计功能代码开发

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -1,11 +1,13 @@
 package com.sky.service.impl;
 
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
 import com.sky.mapper.UserMapper;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import org.apache.commons.lang3.StringUtils;
@@ -19,6 +21,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ReportServiceImpl implements ReportService {
@@ -31,6 +34,7 @@ public class ReportServiceImpl implements ReportService {
 
     /**
      * 统计指定区间内的营业额数据
+     *
      * @param begin
      * @param end
      * @return
@@ -38,27 +42,27 @@ public class ReportServiceImpl implements ReportService {
     @Override
     public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end) {
 
-        //获取日期列表
+        // 获取日期列表
         List<LocalDate> dateList = getDateList(begin, end);
 
-        //获取营业额列表
+        // 获取营业额列表
         List<Double> turnoverList = new ArrayList<>();
         for (LocalDate date : dateList) {
-            //查询date日期对应的营业额数据
-            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);    //当天开始时间
-            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);      //当天结束时间
+            // 查询date日期对应的营业额数据
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);    // 当天开始时间
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);      // 当天结束时间
 
-            //统计时间在当天且订单状态为已完成的订单总营业额
+            // 统计时间在当天且订单状态为已完成的订单总营业额
             Map map = new HashMap();
             map.put("begin", beginTime);
             map.put("end", endTime);
             map.put("status", Orders.COMPLETED);
             Double turnover = orderMapper.sumByMap(map);
-            turnover = turnover == null? 0.0: turnover;
+            turnover = turnover == null ? 0.0 : turnover;
             turnoverList.add(turnover);
         }
 
-        //封装VO对象
+        // 封装VO对象
         TurnoverReportVO turnoverReportVO = TurnoverReportVO.builder()
                 .dateList(StringUtils.join(dateList, ","))
                 .turnoverList(StringUtils.join(turnoverList, ","))
@@ -69,6 +73,7 @@ public class ReportServiceImpl implements ReportService {
 
     /**
      * 统计指定区间内的用户数据
+     *
      * @param begin
      * @param end
      * @return
@@ -76,26 +81,26 @@ public class ReportServiceImpl implements ReportService {
     @Override
     public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
 
-        //获取日期列表
+        // 获取日期列表
         List<LocalDate> dateList = getDateList(begin, end);
 
-        //获取新增用户数列表 select count(id) from user where create_time < ? and create_time > ?
+        // 获取新增用户数列表 select count(id) from user where create_time < ? and create_time > ?
         List<Integer> newUserList = new ArrayList<>();
-        //获取总用户量列表 select count(id) from user where create_time < ?
+        // 获取总用户量列表 select count(id) from user where create_time < ?
         List<Integer> totalUserList = new ArrayList<>();
 
         for (LocalDate date : dateList) {
-            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);    //当天开始时间
-            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);      //当天结束时间
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);    // 当天开始时间
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);      // 当天结束时间
 
             Map map = new HashMap();
 
-            //总用户统计
+            // 总用户统计
             map.put("end", endTime);
             Integer totalUser = userMapper.countByMap(map);
             totalUserList.add(totalUser);
 
-            //新增用户统计
+            // 新增用户统计
             map.put("begin", beginTime);
             Integer newUser = userMapper.countByMap(map);
             newUserList.add(newUser);
@@ -112,6 +117,7 @@ public class ReportServiceImpl implements ReportService {
 
     /**
      * 统计指定区间内的订单数据
+     *
      * @param begin
      * @param end
      * @return
@@ -119,43 +125,43 @@ public class ReportServiceImpl implements ReportService {
     @Override
     public OrderReportVO getOrdersStatistics(LocalDate begin, LocalDate end) {
 
-        //获取日期列表
+        // 获取日期列表
         List<LocalDate> dateList = getDateList(begin, end);
 
-        //有效订单数列表
+        // 有效订单数列表
         List<Integer> validOrderCountList = new ArrayList<>();
-        //订单数列表
+        // 订单数列表
         List<Integer> orderCountList = new ArrayList<>();
 
-        //查询每天的有效订单数和订单总数
+        // 查询每天的有效订单数和订单总数
         for (LocalDate date : dateList) {
 
-            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);    //当天开始时间
-            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);      //当天结束时间
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);    // 当天开始时间
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);      // 当天结束时间
 
-            //查询每天的有效订单数 select count(id) from orders where order_time > ? and order_time < ? and status = 5
+            // 查询每天的有效订单数 select count(id) from orders where order_time > ? and order_time < ? and status = 5
             Integer validOrderCountDay = getOrderCount(beginTime, endTime, Orders.COMPLETED);
 
-            //查询每天的订单总数 select count(id) from orders where order_time > ? and order_time < ?
+            // 查询每天的订单总数 select count(id) from orders where order_time > ? and order_time < ?
             Integer totalOrderCountDay = getOrderCount(beginTime, endTime, null);
 
             validOrderCountList.add(validOrderCountDay);
             orderCountList.add(totalOrderCountDay);
         }
 
-        //计算时间区间内的有效订单数量
+        // 计算时间区间内的有效订单数量
         Integer validOrderCount = validOrderCountList.stream().reduce(Integer::sum).get();
 
-        //计算时间区间内的订单总数量
+        // 计算时间区间内的订单总数量
         Integer totalOrderCount = orderCountList.stream().reduce(Integer::sum).get();
 
-        //计算订单完成率
+        // 计算订单完成率
         Double orderCompletionRate = 0.0;
         if (totalOrderCount != 0) {
             orderCompletionRate = validOrderCount.doubleValue() / totalOrderCount;
         }
 
-        //封装VO对象
+        // 封装VO对象
         OrderReportVO orderReportVO = OrderReportVO.builder()
                 .dateList(StringUtils.join(dateList, ","))
                 .validOrderCountList(StringUtils.join(validOrderCountList, ","))
@@ -169,7 +175,38 @@ public class ReportServiceImpl implements ReportService {
     }
 
     /**
+     * 统计指定区间内的销量排名top10
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
+    @Override
+    public SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end) {
+
+        LocalDateTime beginTime = LocalDateTime.of(begin, LocalTime.MIN);    // 最早时间
+        LocalDateTime endTime = LocalDateTime.of(end, LocalTime.MAX);      // 最晚时间
+
+        // 封装数据表
+        List<GoodsSalesDTO> salesTop10List = orderMapper.getSalesTop10(beginTime, endTime);
+
+        // 数据表按name转成集合
+        List<String> names = salesTop10List.stream().map(GoodsSalesDTO::getName).collect(Collectors.toList());
+        // 商品名称列表
+        String nameList = StringUtils.join(names, ",");
+
+        // 数据表按number转成集合
+        List<Integer> numbers = salesTop10List.stream().map(GoodsSalesDTO::getNumber).collect(Collectors.toList());
+        // 销量列表
+        String numberList = StringUtils.join(numbers, ",");
+
+        // 封装VO对象
+        return new SalesTop10ReportVO(nameList, numberList);
+    }
+
+    /**
      * 获取日期列表
+     *
      * @param begin
      * @param end
      * @return
@@ -189,6 +226,7 @@ public class ReportServiceImpl implements ReportService {
 
     /**
      * 根据条件查询订单数
+     *
      * @param begin
      * @param end
      * @param status
